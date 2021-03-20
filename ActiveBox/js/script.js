d0bf15d89cf140d9cc96c541bfd5aa222f2138d7
Aleksandr Tloka <aleksandr.tloka@i.ua>
2021-03-20 21:26:26 +0200

    finished

--- ActiveBox/js/script.js
+++ ActiveBox/js/script.js
@@ -33,6 +33,26 @@ $(document).ready(function() {
         speed: 300,
         slidesToShow: 1,
     });
+
+    $('.promo__btn').on('click', function() {
+        $('.overlay, .modal').fadeIn('slow');
+    });
+    $('.modal__close').on('click', function() {
+        $('.overlay, .modal').fadeOut('slow');
+    });
+    $(window).scroll(function() {
+        if($(this).scrollTop() > 1600) {
+            $('.pageup').fadeIn() 
+        } else {
+            $('.pageup').fadeOut();
+        }
+    });
+    $("a[href^='#']").click(function() {
+        var _href = $(this).attr("href");
+        $("html, body").animate({scrollTop: 
+        $(_href).offset().top + "px"});
+        return false;
+    });
 });
     
 
